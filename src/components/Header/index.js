541413e483166e7302d6fe0c81c5844aba30b80e
凌 <myywlc@163.com>
2019-06-21 12:49:31 +0800

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Row, Col } from "antd";
+import urils from '../../utils/utils'
+import axios from '../../axios'
+import './index.less'
+
+export default class Header extends Component {
+	state = {
+		userName: '',
+		sysTime: 0,
+		dayPictureUrl: '',
+		weather: '',
+	};
+
+	componentDidMount() {
+		this.setState({
+			userName: 'lin'
+		});
+		setInterval(() => {
+			let sysTime = urils.formatDate(new Date().getTime());
+			this.setState({
+				sysTime
+			})
+		}, 1000);
+
+		this.getWeatherApiData();
+	}
+
+	getWeatherApiData = () => {
+		let city = '杭州';
+		axios.jsonp({
+			url: 'http://api.map.baidu.com/telematics/v3/weather?location=' + encodeURIComponent(city) + '&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+		}).then((res) => {
+			if (res.status === 'success') {
+				let data = res.results[0].weather_data[0];
+				this.setState({
+					dayPictureUrl: data.dayPictureUrl,
+					weather: data.weather
+				})
+			}
+		})
+	};
+
+	render() {
+		const { userName, sysTime, dayPictureUrl, weather } = this.state;
+
+		return (
+			<div className={'header'}>
+				<Row className={'header-top'}>
+					<Col span={24}>
+						<span>hello, {userName}</span>
+						<a href="#">退出</a>
+					</Col>
+				</Row>
+				<Row className={'breadcrumb'}>
+					<Col span={4} className={'breadcrumb-title'}>
+						首页
+					</Col>
+					<Col span={20} className={'weather'}>
+						<span className={'date'}>{sysTime}</span>
+						<span className={'weather-img'}>
+							<img src={dayPictureUrl} alt=""/>
+						</span>
+						<span className={'weather-detail'}>
+							{weather}
+						</span>
+					</Col>
+				</Row>
+			</div>
+		)
+	}
+}
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Menu } from "antd";
+import { NavLink } from 'react-router-dom'
+import menuConfig from '../../config/menuConfig'
+import './index.less'
+
+const SubMenu = Menu.SubMenu;
+const Item = Menu.Item;
+
+export default class NavLeft extends Component {
+	state = {
+		menuTreeNode: null
+	};
+
+	componentDidMount() {
+		const menuTreeNode = this.renderMenu(menuConfig);
+		this.setState({
+			menuTreeNode
+		})
+	}
+
+	renderMenu = data => data.map((item) => {
+		if (item.children) {
+			return (
+				<SubMenu title={item.title} key={item.key}>
+					{this.renderMenu(item.children)}
+				</SubMenu>
+			)
+		}
+		return <Item title={item.title} key={item.key}><NavLink to={'/admin' + item.key}>{item.title}</NavLink></Item>
+	});
+
+	render() {
+		return (
+			<div>
+				<div className={'logo'}>
+					<img src="/assets/logo-ant.svg" alt=""/>
+					<h1>i love you</h1>
+				</div>
+				<Menu theme="dark">
+					{this.state.menuTreeNode}
+				</Menu>
+			</div>
+		)
+	}
+}
