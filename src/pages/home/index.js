6126f65878f7b869f79406707ba6dccc3e224a0c
凌 <myywlc@163.com>
2019-07-28 20:33:33 +0800

    fix css module

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import styles from './index.module.less';
 
+const data = [
+  {
+    label: 'debugId',
+    value: 123456,
+  },
+  {
+    label: 'debugId',
+    value: 123456,
+  },
+  {
+    label: 'debugId',
+    value: 123456,
+  },
+  {
+    label: 'debugId',
+    value: 123456,
+  },
+  {
+    label: 'debugId',
+    value: 123456,
+  },
+  {
+    label: 'debugId',
+    value: 123456,
+  },
+];
+
 export default class Home extends Component {
 
   render() {
     return (
-      <div className={styles["home-wrap"]}>
-        home
+      <div className={styles['home-wrap']}>
+        <div className={styles['box']}>
+          <div className={styles['title']}>
+            <span>Debug</span>
+          </div>
+
+          <div className={styles.content}>
+            {
+              data.map((item, index) => {
+                return (
+                  <div className={styles.item} key={index}>
+                    <span>{item.label} : </span>
+                    <span className={styles.key}>{item.value}</span>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
     );
   }
