b3b89503b3a0baf8c5b96a4764dd5aca6fe88382
凌 <myywlc@163.com>
2019-08-01 01:04:44 +0800

    fix

--- src/pages/ui/collapse/index.js
+++ src/pages/ui/collapse/index.js
@@ -1,9 +1,66 @@
 import React, { PureComponent } from 'react';
+import cx from 'classnames'
+import { Button, Icon } from 'antd';
+import styles from './index.module.less';
 
-export default class Collapse extends PureComponent {
+class Collapse extends PureComponent {
+  state = {
+    open: false,
+  };
+
+  handleClick = () => {
+    const { open } = this.state;
+    this.setState({
+      open: !open,
+    });
+  };
+
+  render() {
+    const { open } = this.state;
+    const { title } = this.props;
+
+    return (
+      <div className={styles.collapse}>
+        <div onClick={this.handleClick} className={cx({[styles.handle]: true, [styles.borderChange]: !open})}>
+          {
+            open ? (
+              <Icon type="down" />
+            ) : (
+              <Icon type="up" />
+            )
+          }
+          <span className={styles.title}>{title}</span>
+        </div>
+        {
+          open && (
+            <div className={styles.content}>
+              {
+                this.props.children
+              }
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default class CollapsePanel extends PureComponent {
   render() {
     return (
-      <div>Collapse</div>
+      <div style={{ marginTop: 80 }}>
+        <Collapse title={'hahaha'}>
+          <Button>123</Button>
+        </Collapse>
+
+        <Collapse title={'hahaha'}>
+          <Button>123</Button>
+        </Collapse>
+
+        <Collapse title={'hahaha'}>
+          <Button>123</Button>
+        </Collapse>
+      </div>
     );
   }
 }
