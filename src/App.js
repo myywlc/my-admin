02712b5658b3c1451df73526da2e0b40e54d0d1b
wb-jcb582641 <wb-jcb582641@alibaba-inc.com>
2019-10-15 12:04:41 +0800

    fix 调整文件层级

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
+import RouterView from './router';
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
-        {this.props.children}
+        <RouterView />
       </Fragment>
     );
   }
--- src/layout/admin.js
+++ src/layout/admin.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import NavLeft from './components/NavLeft';
-import './style/common.less';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import NavLeft from '../components/NavLeft';
+import '../style/common.less';
 
 export default class Admin extends Component {
   render() {
